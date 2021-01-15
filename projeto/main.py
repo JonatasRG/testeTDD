973e9858ad08168dc155cb20a91351fb09a31b57
JonatasRG <70361185+JonatasRG@users.noreply.github.com>
2021-01-15 09:54:33 -0300

    Update main.py

--- projeto/main.py
+++ projeto/main.py
@@ -2,9 +2,17 @@ class lerConta:
     def __init__(self, conta):
         self.conta = conta
 
-    def coloca_1x(self):
-        self.conta=self.conta[:]
-        self.conta.find("(")
+    def coloca_x_entre_numero_e_o_parantes(self):
+        #self.conta=self.conta[:]
+
+        abre_paranteses=self.conta.find("(")
+        while (abre_paranteses >=0)  and (abre_paranteses < len(self.conta)):
+            e_numero= self.conta[abre_paranteses - 1].isnumeric()
+            e_fecha_parentes = True if self.conta[abre_paranteses - 1] == ")" else False
+            if e_numero or e_fecha_parentes:
+                self.conta=self.conta[:abre_paranteses]+"x"+self.conta[abre_paranteses:]
+
+            abre_paranteses = self.conta.find("(",abre_paranteses+1)
 
 
 
