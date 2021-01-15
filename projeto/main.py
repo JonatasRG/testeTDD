5be8b5527c887c0156edabb1e46bf68ecdca12f0
JonatasRG <70361185+JonatasRG@users.noreply.github.com>
2021-01-15 08:59:05 -0300

    Update main.py

--- projeto/main.py
+++ projeto/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+class lerConta:
+    def __init__(self, conta):
+        self.conta = conta
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def coloca_1x(self):
+        self.conta=self.conta[:]
+        self.conta.find("(")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
