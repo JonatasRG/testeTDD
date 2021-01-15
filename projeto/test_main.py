ce8f4fa3149bb7eca1ae44a2399da0d33301a068
JonatasRG <70361185+JonatasRG@users.noreply.github.com>
2021-01-15 09:54:29 -0300

    Update test_main.py

--- projeto/test_main.py
+++ projeto/test_main.py
@@ -4,8 +4,17 @@ from projeto.main import lerConta
 
 
 class TestlerConta(TestCase):
-    def test_coloca_1x(self):
+    def test_coloca_x_entre_numero_e_o_parantes(self):
         conta_testada=lerConta("4(4-1)")
-        conta_testada.coloca_1x()
+        conta_testada.coloca_x_entre_numero_e_o_parantes()
         self.assertEqual(conta_testada.conta,"4x(4-1)")
-        #self.fail()
+
+    def test_coloca_x_entre_numeros_e_os_parantes(self):
+        conta_testada=lerConta("4(4-1)-5(4-2)")
+        conta_testada.coloca_x_entre_numero_e_o_parantes()
+        self.assertEqual(conta_testada.conta,"4x(4-1)-5x(4-2)")
+
+    def test_coloca_x_entre_fecha_e_abre_parantes(self):
+        conta_testada=lerConta("(4-1)(4-2)")
+        conta_testada.coloca_x_entre_numero_e_o_parantes()
+        self.assertEqual(conta_testada.conta,"(4-1)x(4-2)")
