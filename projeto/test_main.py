8610325862fe4bb2cff74ca9b5a6bf2abca0fa75
JonatasRG <70361185+JonatasRG@users.noreply.github.com>
2021-01-16 14:02:57 -0300

    Update test_main.py

--- projeto/test_main.py
+++ projeto/test_main.py
@@ -5,16 +5,24 @@ from projeto.main import lerConta
 
 class TestlerConta(TestCase):
     def test_coloca_x_entre_numero_e_o_parantes(self):
-        conta_testada=lerConta("4(4-1)")
+        conta_testada = lerConta("4(4-1)")
         conta_testada.coloca_x_entre_numero_e_o_parantes()
-        self.assertEqual(conta_testada.conta,"4x(4-1)")
+        self.assertEqual(conta_testada.conta, "4x(4-1)")
 
     def test_coloca_x_entre_numeros_e_os_parantes(self):
-        conta_testada=lerConta("4(4-1)-5(4-2)")
+        conta_testada = lerConta("4(4-1)-5(4-2)")
         conta_testada.coloca_x_entre_numero_e_o_parantes()
-        self.assertEqual(conta_testada.conta,"4x(4-1)-5x(4-2)")
+        self.assertEqual(conta_testada.conta, "4x(4-1)-5x(4-2)")
 
     def test_coloca_x_entre_fecha_e_abre_parantes(self):
-        conta_testada=lerConta("(4-1)(4-2)")
+        conta_testada = lerConta("(4-1)(4-2)")
         conta_testada.coloca_x_entre_numero_e_o_parantes()
-        self.assertEqual(conta_testada.conta,"(4-1)x(4-2)")
+        self.assertEqual(conta_testada.conta, "(4-1)x(4-2)")
+
+
+class TestlerConta(TestCase):
+    def test_transforma_subtracao_em_soma_de_negativo(self):
+        conta_testada = lerConta("(4-1)(4-2)")
+        conta_testada.transforma_subtracao_em_soma_de_negativo()
+        self.assertEqual(conta_testada.conta, "(4+-1)(4+-2)")
+
